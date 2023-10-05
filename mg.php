ef992c66d705ff7e5b0f3599f174d1999ca1fac2
signoutz <aunkaffpom@gmail.com>
2023-10-05 11:58:13 +0700

    asd

--- mg.php
+++ mg.php
@@ -16,9 +16,11 @@ $mysqli->query($sql);
 $rs = $result -> fetch_assoc();
 
 print_r($rs);
-exit;
 
-$sql = "SELECT * FROM `log_data_2561` WHERE `log_id` BETWEEN 24313865 AND 24625804 ORDER BY `log_data_2561`.`log_id` ASC ";
+
+$sql = "SELECT * FROM `log_data_2561` WHERE `log_id` > ".$rs['id']." AND `log_id` <= 24625804 ORDER BY `log_data_2561`.`log_id` ASC limit 1000";
+echo $sql;
+exit;
 $result = $mysqli->query($sql);
 while($row = $result -> fetch_assoc()){
     $log_id = $row['log_id'];
