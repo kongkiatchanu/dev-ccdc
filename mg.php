5583c1422465a5bbc3c5d5606c6b88fea2405c8c
signoutz <aunkaffpom@gmail.com>
2023-10-05 11:35:39 +0700

    s

--- mg.php
+++ mg.php
@@ -1,4 +1,14 @@
 <?php 
+error_reporting(-1);
 
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+$mysqli = new mysqli("localhost","dev","liveboxit","dev");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
 $sql = "SELECT * FROM `log_data_2561` WHERE `log_id` BETWEEN 24313865 AND 24625804 ORDER BY `log_data_2561`.`log_id` ASC ";
 echo $sql;
